ef83ca75af73c0df131c620f73c274b7086aaa2a
Serbanescu Adrian <adi10_00@yahoo.com>
2021-12-30 21:17:43 +0100

    Add files via upload
    Am facut programul mai frumos

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function App() {
 
   return (
-    <div className="app">
-      <h5>Press start button to start the quiz</h5>
-      <Link to="/expert"><button className="btn btn-primary">Start Quiz</button></Link>
+    <div className="position-absolute top-50 start-50 translate-middle text-white">
+      <h1>How safe is my home?</h1>
+      <div className="position-absolute bottom--100 start-50 translate-middle-x">
+        <Link to="/expert"><button className="btn btn-primary">Find out</button></Link>
+    </div>
     </div>
   );
 }
